65131e2d96f0de88a5c23e3f71e6a5b87ab2d04a
Judy McGoldrick <jjeanius@verizon.net>
2018-01-26 02:27:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,18 @@
 class Student
-end
 
 class Student < class User
+
+  def initializes
+    knowledge = []
+  end
+
+  def learn(knowledge)
+    @student_knowledge << knowledge
+  end
+
+  def knowledge
+    @student_knowledge
+  end
+
 end
+    
